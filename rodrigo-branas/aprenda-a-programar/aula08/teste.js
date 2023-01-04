dabdecf3bc5a450a40cfa00251ec76dde5d6c9ad
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-01-04 14:06:14 -0300

    uptade

--- rodrigo-branas/aprenda-a-programar/aula08/teste.js
+++
@@ -1,7 +0,0 @@
-
-console.log(janeiro.totalMensal.saldo);
-console.log(janeiro.totalMensal.saldo === 1989.9);
-console.log(fevereiro.totalMensal.saldo);
-console.log(fevereiro.totalMensal.saldo === 2783.75);
-console.log(marco.totalMensal.saldo);
-console.log(marco.totalMensal.saldo === 3581.57);
