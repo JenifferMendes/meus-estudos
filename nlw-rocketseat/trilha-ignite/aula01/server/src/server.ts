7559c47d56a1f4d52fe1df2bffb148077587afdb
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-01-18 11:30:17 -0300

    feat: uptade aula 02 - trilha ignite
    atualizações back-end

--- nlw-rocketseat/trilha-ignite/aula01/server/src/server.ts
+++ nlw-rocketseat/trilha-ignite/aula01/server/src/server.ts
@@ -1,18 +1,14 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes';
 
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
-    
-    return habits;
-})
+
 
 app.listen({
     port:3333
