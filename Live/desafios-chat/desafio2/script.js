58b7070341acec1d3426b82e562a2408728ead01
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-01-29 16:35:53 -0300

    uptade: modificações nas funções

--- Live/desafios-chat/desafio2/script.js
+++ Live/desafios-chat/desafio2/script.js
@@ -1,13 +1,36 @@
-function onClick() {
-    const bg = document.getElementById("texte");
-    const ball = document.createElement("span");
-    ball.innerText = "close";
-    ball.className = "material-symbols-outlined"
-    bg.appendChild(ball);
-    // bg.style.backgroundColor = 'black';
-}
+// function onClick() {
+//     const bg = document.getElementById("4");
+//     const ball = document.createElement("span");
+//     ball.innerText = "close";
+//     ball.className = "material-symbols-outlined"
+//     bg.appendChild(ball);
+// }
 
 // inicio
-// escolha: bolinha ou x
-// cria a tabela
-// 
+// ESCOLHA x ou o
+// O = "radio_button_unchecked"
+// x = "close"
+//class = "material-symbols-outlined"
+
+function createXouO(id, type) {
+    const place = document.getElementById(id);
+    const create = document.createElement("span");
+    create.innerText = type;
+    create.className = "material-symbols-outlined";
+    place.appendChild(create);
+}
+// createXouO(1, "radio_button_unchecked");
+
+document.addEventListener("click", function(findOutId){
+    const elementId = findOutId.target.id;
+    console.log(elementId);
+});
+
+    // const div = document.querySelector
+
+
+
+
+
+// if("O") {
+// }
