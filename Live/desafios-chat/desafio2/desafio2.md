bf4ca25eb47786770a2cf10e732433b2600ec2d2
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-01-30 13:48:32 -0300

    uptade: funções modificadas!

---
+++ Live/desafios-chat/desafio2/desafio2.md
@@ -0,0 +1,11 @@
+Quando clicar:
+* achar o id do elemento (ok)
+* ver se tem o elemento filho(span) (ok)
+* se não tiver -> criar o elemento(span) (ok)
+* verificar se ganhou
+
+
+
+### verificar se ganhou:
+* ocupar 3 espaços // mesmo texto-tipo)
+
--- Live/desafios-chat/desafio2/script.js
+++ Live/desafios-chat/desafio2/script.js
@@ -1,10 +1,60 @@
-// function onClick() {
-//     const bg = document.getElementById("4");
-//     const ball = document.createElement("span");
-//     ball.innerText = "close";
-//     ball.className = "material-symbols-outlined"
-//     bg.appendChild(ball);
+// function findOutId(element) {
+//     const elementId = element.target.id;
+//     console.log(elementId);
+//     return elementId;
 // }
+// document.addEventListener("click", findOutId);
+
+document.addEventListener("click", function(findOutId){
+const elementId = findOutId.target.id;
+console.log(elementId);
+const bg = document.getElementById(`${elementId}`);
+const span = bg.querySelector(".material-symbols-outlined");
+    if (span) {
+        console.log("já tem");
+    } else {
+    const createX = document.createElement("span");
+    createX.innerText = "close";
+    createX.className = "material-symbols-outlined"
+    bg.appendChild(createX);
+    };
+    if (elementId == 1) {
+        const bga = document.getElementById("5")
+        const createO = document.createElement("span")
+        createO.innerText = "radio_button_unchecked";
+        createO.className = "material-symbols-outlined";
+        bga.appendChild(createO); 
+    }
+    if (elementId == 2) {
+        const bga = document.getElementById("3")
+        const createO = document.createElement("span")
+        createO.innerText = "radio_button_unchecked";
+        createO.className = "material-symbols-outlined";
+        bga.appendChild(createO); 
+    }
+});
+
+// se marcado em tal lugar, o computador marca em lugar especifico;
+
+// const bga = document.getElementById("5")
+// const createO = document.createElement("span")
+// createO.innerText = "radio_button_unchecked";
+// createO.className = "material-symbols-outlined";
+// bga.appendChild(createO);
+// return console.log("criado");
+// if (span )
+
+
+
+// vitória
+
+// const topDiv = document.getElementById(".container-top");
+// const middleDiv = document.getElementById(".container-middle");
+// const bottomDiv = document.getElementById(".container-bottom");
+// if ()
+
+
+
 
 // inicio
 // ESCOLHA x ou o
@@ -12,25 +62,33 @@
 // x = "close"
 //class = "material-symbols-outlined"
 
-function createXouO(id, type) {
-    const place = document.getElementById(id);
-    const create = document.createElement("span");
-    create.innerText = type;
-    create.className = "material-symbols-outlined";
-    place.appendChild(create);
-}
+// function createXouO(id, type) {
+//     const place = document.getElementById(id);
+//     const create = document.createElement("span");
+//     create.innerText = type;
+//     create.className = "material-symbols-outlined";
+//     place.appendChild(create);
+// }
 // createXouO(1, "radio_button_unchecked");
 
-document.addEventListener("click", function(findOutId){
-    const elementId = findOutId.target.id;
-    console.log(elementId);
-});
-
-    // const div = document.querySelector
 
 
-
-
-
-// if("O") {
+// function findOutId(element) {
+//     const elementId = element.target.id;
+//     console.log(elementId);
+//     return elementId;
 // }
+
+// document.addEventListener("click", findOutId);
+// const bg = document.getElementById(`${elementId}`);
+// //     const span = bg.querySelector(".material-symbols-outlined");
+// // if (span) {
+// //     return console.log("já tem");
+// // } else {
+// //     const ball = document.createElement("span");
+// //     ball.innerText = "close";
+// //     ball.className = "material-symbols-outlined"
+// //     bg.appendChild(ball);
+// //     return console.log("criado");
+// //     };
+// // });
